01daf6d3f2d37df7538d17abe82139ccba71508d
devandrevieira <dev.andrevieira@gmail.com>
2021-10-13 23:36:48 +0100

    Add project files.

---
+++ LessonOopAccountOne/Account.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+
+namespace LessonOopAccountOne
+{
+    class Account
+    {
+        public int NumAccount { get; private set; }
+        public string NameHolder;
+        public char InitialDeposit { get; private set; }
+        public double InitialAmount { get; private set; }
+        public double Balance { get; private set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int numAccount, string nameHolder, char initialDeposit, double initialAmount, double balance)
+        {
+            NumAccount = numAccount;
+            NameHolder = nameHolder;
+            InitialDeposit = initialDeposit;
+            InitialAmount = initialAmount;
+            Balance = balance;
+
+        }
+
+        public void OpenAccount(int numAccount, string nameHolder, char initialDeposit, double initialAmount, double balance)
+        {
+            Console.Write("Enter the account number: ");
+            NumAccount = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the name of the holder: ");
+            NameHolder = (Console.ReadLine());
+
+            Console.Write("Will there be initial deposit ?: (y/n)? ");
+            InitialDeposit = char.Parse(Console.ReadLine());
+
+            if (InitialDeposit == 'y')
+            {
+                Console.Write("Enter the initial amount: ");
+                InitialAmount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                InitialAmount = 0;
+            }
+
+            Balance = InitialAmount;
+        }
+
+        public double Deposit(double deposit)
+        {
+            return Balance += deposit;
+        }
+
+        public double Withdrawn(double withdrawn)
+        {
+            return Balance -= withdrawn + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Account Number: " + NumAccount +
+                ", Holder: " + NameHolder +
+                ", Balance: $ " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ LessonOopAccountOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace LessonOopAccountOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("####### PROGRAM BANK ACCOUNT #######");
+            Console.WriteLine();
+
+            Account account = new Account();
+
+            account.OpenAccount(account.NumAccount, account.NameHolder, account.InitialDeposit, account.InitialAmount, account.Balance);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Account data:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.Write("Enter the deposit amount : ");
+            double deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposit(deposit);
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.Write("Enter the value for the withdrawn: ");
+            double withdrawn = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Withdrawn(withdrawn);
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(account);
+        }
+    }
+}
